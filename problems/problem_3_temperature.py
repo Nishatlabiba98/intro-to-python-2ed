a0461f2f8aa1dab033f691ee52413925df23ea69
Nishat Labiba <nishatlabiba98@gmail.com>
2026-05-28 10:21:55 -0400

    learning at work

--- problems/problem_3_temperature.py
+++ problems/problem_3_temperature.py
@@ -1,8 +1,9 @@
 """
 Convert a temperature from Fahrenheit to Celsius
 """
-input("Temp in °F: ")
-
+temp_f = input("Temp in °F: ")
+temp_f = int (temp_f)
 # Calculate it in Celsius
-
-print("Temp in °C: ")
+temp_c = (temp_f - 32) * 5/9
+temp_c = str (temp_c)
+print("Temp in °C: " + temp_c)
